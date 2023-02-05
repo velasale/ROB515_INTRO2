7cc4b4a211887a5bbb64b7b552af54d968014584
Alejandro Velasquez <velasale@oregonstate.edu>
2023-02-04 19:29:13 -0800

    added controller within the concurrency

--- picar-x/picarx/velasale_week4.py
+++ picar-x/picarx/velasale_week4.py
@@ -483,7 +483,7 @@ def week_3(px, sensor="photosensor"):
             # Step 2: Control
             px.set_dir_servo_angle(steer_angle)
             print("The commanded steer angle is: %.2f" % steer_angle)
-            px.forward(1)
+            # px.forward(1)
 
             e_time = time.time() - start
 
