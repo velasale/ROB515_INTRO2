0493b685fb62f9bff23d8da3d17b5903b5c501d7
Alejo <velasale@oregonstate.edu>
2023-03-07 17:04:22 -0800

    Perception Refactoring

--- pi-arm/ArmPi/ROB515_REFACTOR/arm_perception.py
+++ pi-arm/ArmPi/ROB515_REFACTOR/arm_perception.py
@@ -38,7 +38,6 @@ class ArmSensing():
         if img is not None:
             self.cross_hair()
             frame_lab = self.filter()
-
             return frame_lab
 
     def cross_hair(self):
@@ -51,17 +50,65 @@ class ArmSensing():
         """Applies filter"""
         frame_resize = cv2.resize(self.img, self.task.size, interpolation=cv2.INTER_NEAREST)
         frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
+
+        # TODO (line 346) of ColorTracking.py
+        if self.get_roi and self.start_pick_up:
+            self.get_roi = False
+            frame_gb = getMaskROI(frame_gb, roi, size)
+
         frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # convert image to lab space
 
         return frame_lab
 
 
-
 class ArmInterpretation():
     """ This class and its methods return the x,y location of the object"""
 
-    def __init__(self):
+    def __init__(self, task):
+        self.task = task
+        self.area_max = 0
+        self.areaMaxContour = 0
+        self.rect = None
 
+
+    def function(self):
+
+        self.area_max = 0
+        self.areaMaxContour = 0
+        if not self.task.start_pick_up:
+            for i in color_range:  #color range comes from LABconfig.py
+                if i in self.task.__target_color:
+                    detect_color = i
+
+                    # Perform bitwise operations on original image and mask
+                    frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][1])
+                    # Open Operation
+                    opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))
+                    # Close Operation
+                    closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))
+                    # Find the outline
+                    contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]
+                    # Find the largest contour
+                    self.reaMaxContour, self.area_max = getAreaMaxContour(contours)
+
+            if self.area_max > 2500:
+                self.rect = cv2.minAreaRect(areaMaxContour)
+                self.box = np.int0(cv2.boxPoints(rect))
+
+                # TODO
+                self.roi = getROI(self.box)
+                self.get_roi = True
+
+                # Get the coordinates of the center of the block
+                img_centerx, img_centery = getCenter(self.rect, self.roi, self.task.size, square_length)
+                # Convert to real world coordinates
+                world_x, world_y = convertCoordinate(img_centerx, img_centery, size)
+
+                # Draw Contours
+                cv2.drawContours(img, [box], -1, range_rgb[detect_color], 2)
+                cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')',
+                            (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, range_rgb[detect_color], 1)  # draw center point
 
 
 class ArmController():
@@ -90,7 +137,7 @@ class ArmTask():
         self.__target_color = ('red')
         self._stop = False
 
-        # Perception Variables
+        # Perception Parameters
         self.range_rgb = {
             'red': (0, 0, 255),
             'blue': (255, 0, 0),
@@ -98,8 +145,9 @@ class ArmTask():
             'black': (0, 0, 0),
             'white': (255, 255, 255)
         }
-        self.rect = None
         self.size = (640, 480)
+
+
 
         # Actuation Variables
         self.rotation_angle = 0
@@ -107,6 +155,11 @@ class ArmTask():
         self.world_X, self.world_Y = 0, 0
         self.world_x, self.world_y = 0, 0
         self.last_x, self.last_y = 0, 0
+
+
+    def reset(self):
+        ...
+        # TODO
 
 
 
