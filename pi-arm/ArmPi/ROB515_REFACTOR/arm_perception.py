f6254fe267fc157b3c3dd2821ab28c9dc6781aa7
Alejo <velasale@oregonstate.edu>
2023-03-10 11:53:10 -0800

    Perception Refactoring

--- pi-arm/ArmPi/ROB515_REFACTOR/arm_perception.py
+++ pi-arm/ArmPi/ROB515_REFACTOR/arm_perception.py
@@ -189,8 +189,8 @@ class ImageVisualizer():
 
     def function(self, image):
         print("Displaying image")
-        cv2.imshow('Frame', image[0])
-        key = cv2.waitKey(1)
+        cv2.imshow('Frame', image[1])
+        key = cv2.waitKey(100)
         cv2.destroyAllWindows()
 
 
