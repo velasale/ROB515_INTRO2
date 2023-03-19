3ab5641989a35f50726725d4eee5fb402df8661c
Alejo <velasale@oregonstate.edu>
2023-03-19 14:06:25 -0700

    Perception Refactoring

--- pi-arm/ArmPi/ROB515_REFACTOR/arm_perception.py
+++ pi-arm/ArmPi/ROB515_REFACTOR/arm_perception.py
@@ -413,7 +413,7 @@ def main():
     # Concurrent Execution
     producer_consumer_list = [wSensor,
                               wInterpreter,
-                              # wController,
+                              wController,
                               wDisplay
                               ]
 
