a809f8659708d724d6527122deeb1eee606a8d90
Alejo <velasale@oregonstate.edu>
2023-03-19 17:12:37 -0700

    Perception Refactoring

--- pi-arm/ArmPi/ROB515_REFACTOR/arm_perception.py
+++ pi-arm/ArmPi/ROB515_REFACTOR/arm_perception.py
@@ -363,7 +363,7 @@ def main():
 
     # Instances of Buses
     print('Instances of buses')
-    bSensor = rr.Bus(sensor.function(), "Camera Sensor Bus")
+    bSensor = rr.Bus(sensor.function(0), "Camera Sensor Bus")
     bInterpreter = rr.Bus(interpreter.function(bSensor.message), "Interpreter Sensor Bus")
     bController = rr.Bus(controller.function(bInterpreter.message), "Controller Sensor Bus")
     bTerminate = rr.Bus(0, "Termination Bus")
