2782857a59d40294073cdecb98960eb64ced5b5c
Alejo <velasale@oregonstate.edu>
2023-03-23 10:01:18 -0700

    basic flow of car loading function

--- pi-car/picar-x/picarx/velasale_week5.py
+++ pi-car/picar-x/picarx/velasale_week5.py
@@ -553,7 +553,7 @@ def week_5(px):
     graySensor = rr.Producer(
         sensor.adc_list,                # function that will generate data
         bGraySensor,                    # output data bus
-        0.001,                           # delay between data generation
+        0.0001,                           # delay between data generation
         bTerminate,                     # bus to watch for termination signal
         "Read GrayScale sensor signal")
 
@@ -561,28 +561,28 @@ def week_5(px):
         interpreter.sharp_edge,     # function that processes data
         bGraySensor,                    # input data bus
         bGrayInterpreter,               # output data bus
-        0.001,                           # delay
+        0.0001,                           # delay
         bTerminate,                     # bus to watch for termination signal
         "Interpret Grayscale into line position")
 
     ultrasonicSensor = rr.Producer(
         ultrasonic.read,
         bUltrasonic,
-        0.02,
+        0.05,
         bTerminate,
         "Read and Interpret Ultrasound distance")
 
     dirController = rr.Consumer(
         controller.steer_towards_line,
         bGrayInterpreter,
-        0.001,
+        0.0001,
         bTerminate,
         "Control steering angle")
 
     distController = rr.Consumer(
         dController.move_stop,
         bUltrasonic,
-        0.02,
+        0.05,
         bTerminate,
         "Watching the distance ahead")
 
